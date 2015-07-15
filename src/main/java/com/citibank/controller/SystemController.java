331fcf28e12e0b367c4605f8b34e0f80d165fbd9
liwang112 <liwangadd@163.com>
2015-07-15 12:29:46 +0800

    for pull

--- src/main/java/com/citibank/controller/SystemController.java
+++ src/main/java/com/citibank/controller/SystemController.java
@@ -1,10 +1,7 @@
 package com.citibank.controller;
 
-import net.sf.json.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,7 +14,8 @@ import java.util.Map;
 public class SystemController {
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public @ResponseBody Map<String, String> index(){
+    public @ResponseBody Map<String, String> index(@RequestHeader Map<String, String> headers){
+        System.out.println(headers.toString());
         Map map = new HashMap<String, String>();
         map.put("name", "ssss");
         map.put("school", "ssdut");
