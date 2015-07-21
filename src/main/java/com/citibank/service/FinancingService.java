e617adb7d0f61c20dedda4625fc4146d58102470
269284999@qq.com <269284999@qq.com>
2015-07-21 20:30:07 +0800

    Merge branch 'master' of https://git.oschina.net/liwang112/CitiBankBg
    Conflicts:
    	src/main/java/com/citibank/controller/SystemController.java
    	src/main/resources/data-access.properties
    	src/main/webapp/views/main/index.jsp

---
+++ src/main/java/com/citibank/service/FinancingService.java
@@ -0,0 +1,34 @@
+package com.citibank.service;
+
+
+import com.citibank.dao.Page;
+
+import java.util.Map;
+
+/**
+ * Created by zjm on 2015/7/21.
+ */
+public interface FinancingService {
+    //撮合配对
+
+    public Page<Map<String,Object>> getMatching(String userId,int userType,int pageIndex,String investArea,String investIndustry,int fundBody,int lowMoney, int highMoney);
+
+
+
+
+
+
+    //意向发布
+
+
+    //业务洽谈
+
+
+    //电子签约
+
+
+
+
+
+
+}
