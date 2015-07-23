6e384c4a881d1fe90cc48ff5464670c3ecd91f05
269284999@qq.com <269284999@qq.com>
2015-07-23 16:32:02 +0800

    Merge branch 'master' of https://git.oschina.net/liwang112/CitiBankBg
    Conflicts:
    	src/main/java/com/citibank/service/impl/AssetServiceImpl.java

--- src/main/java/com/citibank/controller/SystemController.java
+++ src/main/java/com/citibank/controller/SystemController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.*;
 
@@ -19,15 +20,14 @@ public class SystemController {
     @Autowired
     private MySQLSimpleDaoImpl mySQLSimpleDao;
 
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public @ResponseBody
-    int index(){
-        Map<String, Object> map=new HashMap<String, Object>();
-        map.put("inTime", new Date());
-//        mySQLSimpleDao.create("person",map);
-        String sql="select * from PERSON where id = 1";
-        List<Map<String, Object>> result = mySQLSimpleDao.queryForList(sql, new HashMap<String, Objects>());
-        Date date= (Date) result.get(0).get("in_time");
-        return date.getMonth();
+
+
+    @RequestMapping(value = "/index",method = RequestMethod.POST)
+    public ModelAndView test(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("sss","123");
+        return modelAndView;
     }
+
+
 }
