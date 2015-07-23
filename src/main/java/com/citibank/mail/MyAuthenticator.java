e7fb17104da538a11973347536cf5c6da789147e
liwang112 <liwangadd@163.com>
2015-07-23 17:09:11 +0800

    格式化邮件模块

--- src/main/java/com/citibank/mail/MyAuthenticator.java
+++ src/main/java/com/citibank/mail/MyAuthenticator.java
@@ -3,19 +3,20 @@ package com.citibank.mail;
 import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
 
-public class MyAuthenticator extends Authenticator{   
-    String userName=null;   
-    String password=null;   
-        
-    public MyAuthenticator(){   
-    	
-    }   
-    public MyAuthenticator(String username, String password) {    
-        this.userName = username;    
-        this.password = password;    
-    }    
-    
-    protected PasswordAuthentication getPasswordAuthentication(){   
-        return new PasswordAuthentication(userName, password);   
-    }   
+public class MyAuthenticator extends Authenticator {
+    String userName = null;
+    String password = null;
+
+    public MyAuthenticator() {
+
+    }
+
+    public MyAuthenticator(String username, String password) {
+        this.userName = username;
+        this.password = password;
+    }
+
+    protected PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication(userName, password);
+    }
 }   
